7366b94098fc6b1357b486bdb029e126d1c918a5
Todd Pangilinan <pang.todd@gmail.com>
2023-01-28 15:45:02 -0800

    established file and inserted instruction. WIP for problem.

---
+++ divSum.js
@@ -0,0 +1,8 @@
+// Given an array of integers and a positive integer K, determine the number of (i,j) pairs where i< j and ar[i] + ar[j]  is divisible by k.
+
+// Example:
+
+// ar = [1, 2, 3, 4, 5, 6]
+// k = 5
+
+// Three pairs meet the criteria:  [1, 4] [2, 3] and [4, 6]. all create sums divisible by 5, and 1st position is < 2nd position in the array.
