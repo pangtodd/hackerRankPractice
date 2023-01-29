401a35f6d08fd6e634b8c9c5d70cd7514336aee2
Todd Pangilinan <pang.todd@gmail.com>
2023-01-28 19:31:06 -0800

    came up with a solution that passes inital case. Fails other cases. WIP.

--- divSum.js
+++ divSum.js
@@ -6,3 +6,18 @@
 // k = 5
 
 // Three pairs meet the criteria:  [1, 4] [2, 3] and [4, 6]. all create sums divisible by 5, and 1st position is < 2nd position in the array.
+
+function divisibleSumPairs(n, k, ar) {
+  // Write your code here
+  let answer= [];
+  ar.forEach(i=>{
+      ar.forEach(j=>{
+          let success= [];
+          if ((i + ar[j]) % k === 0 && i < ar[j]){
+              success.push(i, ar[j]);
+              answer.push(success);
+          }
+      });
+  });
+  return answer.length;
+}
