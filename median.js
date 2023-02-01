68b853442fd9a3fa60ab55e3d7704273aec9e3f4
Todd Pangilinan <pang.todd@gmail.com>
2023-02-01 14:24:11 -0800

    bugfix, got it passing all tests by updating sort.

--- median.js
+++ median.js
@@ -4,8 +4,10 @@
 // arr= [5, 3, 1, 2, 4]
 // The sorted array = [1,2,3,4,5]. The middle element and the median is 3 .
 
-function Median(n){
-  let sortedArr=arr.sort();
+function findMedian(arr) {
+  let sortedArr=arr.sort(function (a, b) {  return a - b;  });
   let position = Math.floor(arr.length/2);
-  return sortedArr[position]
+  return sortedArr[position];
 }
+
+//thrown off by the sorting...forgot .sort() does it "alphabetically" from the first digit.
