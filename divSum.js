17515e0314d5c61a8876876dcc345c3ce6c5a253
Todd Pangilinan <pang.todd@gmail.com>
2023-01-31 16:49:59 -0800

    prepare for merge.

--- divSum.js
+++ divSum.js
@@ -1,4 +1,4 @@
-// Given an array of integers and a positive integer K, determine the number of (i,j) pairs where i< j and ar[i] + ar[j]  is divisible by k.
+Given an array of integers and a positive integer K, determine the number of (i,j) pairs where i< j and ar[i] + ar[j]  is divisible by k.
 
 // Example:
 
