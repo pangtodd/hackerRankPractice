4d24ca0737c65ddea0c9ac7d5d5d2edab06a16dc
Todd Pangilinan <pang.todd@gmail.com>
2023-02-02 21:05:58 -0800

    intial file and prompt added.

---
+++ flipBit.js
@@ -0,0 +1,8 @@
+//prompt:
+// You will be given a list of 32 bit unsigned integers. Flip all the bits ( 1 and 0) and return the result as an unsigned integer.
+
+// Example
+// n = 1
+//  9 We're working with 32 bits, so:
+// 00000000000000000000000000000001 = 1
+// 11111111111111111111111111111110 = 4294967294
