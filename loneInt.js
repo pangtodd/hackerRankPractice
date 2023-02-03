2218f0c7ea1d7f347f1e38c8fb1e59d71432db9b
Todd Pangilinan <pang.todd@gmail.com>
2023-02-02 18:00:40 -0800

    came up with goofy solution, but it passes.

--- loneInt.js
+++ loneInt.js
@@ -4,3 +4,13 @@
 // a= [1,2,3,4,3,2,1]
 
 // The unique element is 4.
+
+function lonelyinteger(a) {
+  let answer = 69;
+  for(let i = 0; i<a.length; i++){
+      if (a.lastIndexOf(a[i]) === a.indexOf(a[i])) {
+          answer = i;
+      }
+  }
+  return a[answer];
+}
