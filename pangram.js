7fc6b03944f7e8c387e8edc81d76ef17f767f564
Todd Pangilinan <pang.todd@gmail.com>
2023-02-07 16:38:02 -0800

    wrote passing solution, but could make it cleaner with a ternary operator.

--- pangram.js
+++ pangram.js
@@ -1,3 +1,18 @@
 // prompt:
 // A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
 
+function pangrams(s) {
+  const alpha = "abcdefghijklmnopqrstuvwxyz";
+  const sLowerCase= s.toLowerCase();
+  let score = 0;
+  for(let i = 0; i < alpha.length; i++){
+      if (!sLowerCase.includes(alpha[i])){
+          score++;
+      }
+  }
+  if (score>0){
+      return "not pangram"
+  } else {
+      return "pangram"
+  }
+}
