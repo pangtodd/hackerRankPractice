aae338c30c8c5f2286bb3a384db450413fe752cd
Todd Pangilinan <pang.todd@gmail.com>
2023-02-13 20:14:22 -0800

    I think I understand a basic solution, but not sure how to loop it. Going to solve longhand first and then work on refactoring.

--- flippedArray.js
+++ flippedArray.js
@@ -5,3 +5,58 @@
 
 // Given the initial configurations for
 // matrices, help Sean reverse the rows and columns of each matrix in the best possible way so that the sum of the elements in the matrix's upper-left quadrant is maximal. 
+
+function flippingMatrix(matrix) {
+  // Write your code here
+  let rowOne=matrix[0];
+  let rowTwo = matrix[1];
+  let secondToLastRow= matrix[matrix.length-2]
+  let lastRow= matrix[matrix.length-1]
+  
+  let topLeft= 0;
+  let topRight = 0;
+  let bottomRight = 0;
+  let bottomLeft = 0;
+  let placeHolder= 0
+  
+  if (rowOne[0]>rowOne[matrix.length-1]){
+      topLeft = rowOne[0]
+  } else {
+      topLeft = rowOne[matrix.length-1]
+  }
+  
+  if (lastRow[0]>lastRow[matrix.length-1]){
+      let placeHolder= lastRow[0]
+  } else {
+      let placeHolder = lastRow[matrix.length -1]
+  }
+  
+  if (placeHolder>topLeft){
+      topLeft= placeHolder;
+      placeHolder = 0;
+    } else {
+      placeholder=0;
+  }
+  
+  if (rowTwo[0]>rowTwo[matrix.length-1]){
+      bottomLeft = rowTwo[0]
+    } else {
+      bottomLeft = rowTwo[matrix.length-1]
+  }
+  
+  if (secondToLastRow[0]>secondToLastRow[matrix.length-1]){
+      let placeHolder = secondToLastRow[0]
+    } else {
+      let placeHolder = secondToLastRow[matrix.length -1]
+  }
+  
+  if (placeHolder>bottomLeft){
+      bottomLeft=placeHolder;
+      placeHolder=0;
+    } else {
+      placeHolder = 0;
+  }
+
+  
+  
+}
