24c353fc02a483df39b1e77ca354a75bb3a3fbcc
Todd Pangilinan <pang.todd@gmail.com>
2023-02-15 20:26:56 -0800

    finsished my long-winded solution. Passes initial test, but didn't pass the others.

--- flippedArray.js
+++ flippedArray.js
@@ -8,55 +8,88 @@
 
 function flippingMatrix(matrix) {
   // Write your code here
-  let rowOne=matrix[0];
-  let rowTwo = matrix[1];
-  let secondToLastRow= matrix[matrix.length-2]
-  let lastRow= matrix[matrix.length-1]
-  
-  let topLeft= 0;
-  let topRight = 0;
-  let bottomRight = 0;
-  let bottomLeft = 0;
-  let placeHolder= 0
-  
-  if (rowOne[0]>rowOne[matrix.length-1]){
-      topLeft = rowOne[0]
-  } else {
-      topLeft = rowOne[matrix.length-1]
-  }
-  
-  if (lastRow[0]>lastRow[matrix.length-1]){
-      let placeHolder= lastRow[0]
-  } else {
-      let placeHolder = lastRow[matrix.length -1]
-  }
-  
-  if (placeHolder>topLeft){
-      topLeft= placeHolder;
-      placeHolder = 0;
-    } else {
-      placeholder=0;
-  }
-  
-  if (rowTwo[0]>rowTwo[matrix.length-1]){
-      bottomLeft = rowTwo[0]
-    } else {
-      bottomLeft = rowTwo[matrix.length-1]
-  }
-  
-  if (secondToLastRow[0]>secondToLastRow[matrix.length-1]){
-      let placeHolder = secondToLastRow[0]
-    } else {
-      let placeHolder = secondToLastRow[matrix.length -1]
-  }
-  
-  if (placeHolder>bottomLeft){
-      bottomLeft=placeHolder;
-      placeHolder=0;
-    } else {
-      placeHolder = 0;
+const rowOne=matrix[0];
+const rowTwo = matrix[1];
+const secondToLastRow= matrix[matrix.length-2]
+const lastRow= matrix[matrix.length-1]
+
+let topLeft= 0;
+let topRight = 0;
+let bottomRight = 0;
+let bottomLeft = 0;
+let placeHolder= 0;  
+
+// topLeft score
+if (rowOne[0]>rowOne[matrix.length-1]){
+    topLeft = rowOne[0]
+} else {
+    topLeft = rowOne[matrix.length-1]
+}
+
+if (lastRow[0]>lastRow[matrix.length-1]){
+    placeHolder= lastRow[0]
+} else {
+    placeHolder = lastRow[matrix.length -1]
+}
+
+if (placeHolder>topLeft){
+    topLeft= placeHolder;
   }
 
-  
-  
+// bottomLeft score
+if (rowTwo[0]>rowTwo[matrix.length-1]){
+    bottomLeft = rowTwo[0]
+  } else {
+    bottomLeft = rowTwo[matrix.length-1]
+}
+
+if (secondToLastRow[0]>secondToLastRow[matrix.length-1]){
+    placeHolder = secondToLastRow[0]
+  } else {
+    placeHolder = secondToLastRow[matrix.length -1]
+}
+
+if (placeHolder>bottomLeft){
+    bottomLeft=placeHolder;
+  }
+
+// topRight Score
+if (rowOne[1]>rowOne[matrix.length-2]){
+  topRight = rowOne[1]
+} else {
+  topRight = rowOne[matrix.length-2]
+};
+if (lastRow[1]>lastRow[matrix.length-2]){
+  placeHolder= lastRow[1]
+  } else {
+  placeHolder = lastRow[matrix.length -2]
+}
+
+if (placeHolder>topRight){
+  topRight= placeHolder;
+}
+
+// bottomRightscore
+if (rowTwo[1]>rowTwo[matrix.length-2]){
+  bottomRight = rowTwo[1]
+} else {
+  bottomRight = rowTwo[matrix.length-2]
+}
+
+if (secondToLastRow[0]>secondToLastRow[matrix.length-2]){
+  placeHolder = secondToLastRow[0]
+} else {
+  placeHolder = secondToLastRow[matrix.length -2]
+}
+
+if (placeHolder>bottomRight){
+  bottomRight=placeHolder;
+  placeHolder=0;
+} else {
+  placeHolder = 0;
+}
+
+console.log( topLeft, topRight, bottomLeft, bottomRight)
+return ( topLeft + topRight + bottomLeft + bottomRight)
+
 }
