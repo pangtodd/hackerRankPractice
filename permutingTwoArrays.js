df740cfd3a5dd5a37ef359ba3f8cfc6dd7495bb9
Todd Pangilinan <pang.todd@gmail.com>
2023-02-23 10:05:13 -0800

    added prompt/initial file.

---
+++ permutingTwoArrays.js
@@ -0,0 +1,14 @@
+// There are two n-element arrays of integers, A  and B. Permute them into some A' and B' such that the relation A'[i]+ B'[i] ≥ k holds for all i where O ≤ i< n.
+
+// There will be q queries consisting of A, B, and k. For each query, return YES if some permutation A',B' satisfying the relation exists. Otherwise, return NO.
+
+// Example:
+// k= 10
+// A = [2, 1, 3]
+// B = [7, 8, 9]
+// YES
+
+// k=5
+// A= [1, 2, 2, 1]
+// B= [3, 3, 3, 4]
+// NO 
