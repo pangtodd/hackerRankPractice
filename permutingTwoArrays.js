c140b92ef5ae7cb4e6786e5a28a906ac72e149e5
Todd Pangilinan <pang.todd@gmail.com>
2023-02-23 20:24:29 -0800

    wrote working solution.

--- permutingTwoArrays.js
+++ permutingTwoArrays.js
@@ -12,3 +12,27 @@
 // A= [1, 2, 2, 1]
 // B= [3, 3, 3, 4]
 // NO 
+
+function twoArrays(k, A, B) {
+  const sortedA = A.sort(function(a, b) {return a - b;});
+  let sortedB = B.sort(function(a, b) {return a - b;});
+  let score = 0;
+  for (let i = 0; i <A.length; i++){
+      let minSolution = k-sortedA[i];
+      let position = sortedB.findIndex((e) => e >= minSolution);
+      console.log(sortedB);
+      if (position < 0 ){
+          score=+1  
+      } else {
+          sortedB.splice([position],1)
+      }
+  };
+  console.log(score)
+  
+  if (score > 0){
+      return "NO"
+  } else {
+      return "YES"
+  }
+
+}
