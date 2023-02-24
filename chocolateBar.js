225a4c900ab80bb44f1a5ed637d42b394017e34c
Todd Pangilinan <pang.todd@gmail.com>
2023-02-24 11:58:32 -0800

    wrote passing solution.

--- chocolateBar.js
+++ chocolateBar.js
@@ -15,3 +15,16 @@
 // length of choc bar must be 2, the sum of the int must be 4.
 // there are two solutions, [2,2] and [1,3].
 
+function birthday(s, d, m) {
+  let score = 0;
+  for(let i = 0; i<s.length; i++){
+      let segment = s.slice(i, i+m)
+      let segSum= segment.reduce((total, current) => { return total + current;
+  }, 0);
+      if (segment.length === m && segSum === d) {
+          score+=1;
+      }
+  }
+  return score
+}
+
