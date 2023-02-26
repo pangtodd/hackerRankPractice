0ee842b05d1830d71d3f77fb243c845f220aba28
Todd Pangilinan <pang.todd@gmail.com>
2023-02-26 11:07:56 -0800

    establish initial file and prompt.

---
+++ week03/maxTriangle.js
@@ -0,0 +1,13 @@
+// Given an array of stick lengths, use 3 of them to construct a non-degenerate triangle with the maximum possible perimeter. Return an array of the lengths of its sides as 3 integers in non-decreasing order.
+
+// If there are several valid triangles having the maximum perimeter:
+
+// 1. Choose the one with the longest maximum side.
+// 2. If more than one has that maximum, choose from them the one with the longest minimum side.
+// 3. If more than one has that maximum as well, print any one them.
+// If no non-degenerate triangle exists, return [-1].
+
+// Example
+// sticks = [1,2,3,4,5,10]
+
+// The triplet (1,2,3)  will not form a triangle. Neither will (4,5,6) or (2,3,5) so the problem is reduced to (2,3,4) and (3,4,5) . The longer perimeter is (3,4,5).
