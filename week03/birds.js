f228d5177a3ad31015cb1687a8d5bfb899985644
Todd Pangilinan <pang.todd@gmail.com>
2023-02-26 10:51:19 -0800

    got working solution, uses two non-nested loops and stores the values in an object

--- week03/birds.js
+++ week03/birds.js
@@ -5,13 +5,31 @@
 
 // There are two each of types 1 and 2, and one sighting of type 3. Pick the lower of the two types seen twice: type 1.
 
+// first attempt: works, but too slow. Works like having a nested loop, so not very efficent.
+// function migratoryBirds(arr) {
+//   let winningBird=0;
+//   for(let i=0;i<arr.length;i++){
+//       let iArray= arr.filter(bird=>bird === arr[i]);
+//       if (iArray.length>winningBird){
+//           winningBird = arr[i];
+//       };
+//   }
+//   return winningBird;
+// }
+
 function migratoryBirds(arr) {
-  let winningBird=0;
-  for(let i=0;i<arr.length;i++){
-      let iArray= arr.filter(bird=>bird === arr[i]);
-      if (iArray.length>winningBird){
-          winningBird = arr[i];
-      };
+  const counts = {};
+  let score = 0;
+  let winner =0;
+  for (const num of arr) {
+       counts[num] = counts[num] ? counts[num] + 1 : 1;
+  };
+  for (let i=1; i<6; i++){
+      if (counts[i]> score){
+          score = counts[i];
+          winner= i;
+      }
   }
-  return winningBird;
+  return winner;
+  
 }
