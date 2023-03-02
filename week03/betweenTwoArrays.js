57e746cffc84178bea630f60f52ffd13134626fa
Todd Pangilinan <42262107+pangtodd@users.noreply.github.com>
2023-03-01 22:50:13 -0800

    Update betweenTwoArrays.js
    WIP, ugly solution (two separate nested loops). But passes initial two test cases.

--- week03/betweenTwoArrays.js
+++ week03/betweenTwoArrays.js
@@ -15,3 +15,35 @@
 // return 2.
 
 
+function getTotalX(a, b) {
+  let range = [];
+  let aFactors = [];
+  let answers= []
+  for (let i = a[a.length - 1]; i <= b[0]; i++) {
+    if (i % a[0] === 0) {
+      range.push(i);
+    }
+  };
+  console.log(range);
+  for (let j = 0; j < range.length; j++) {
+    for (let k = 1; k < a.length; k++) {
+      if (range[j] % a[k] === 0) {
+        aFactors.push(range[j]);
+      }
+    }
+  }
+  console.log(aFactors)
+
+  for (let l = aFactors.length; l >0; l--){
+     for (let m = b.length; m> 0; m--){
+       if (b[m]%aFactors[l]===0){
+         answers.push(aFactors[l]);
+       }
+     }
+   }
+   
+   const finalAnswer= [...new Set(answers)];
+   console.log("answer"+ finalAnswer)
+
+   return finalAnswer.length;
+}
