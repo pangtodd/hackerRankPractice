96793098f02f2c6cb1a08c16b09470027dfcdb77
Todd Pangilinan <pang.todd@gmail.com>
2023-03-03 23:36:39 -0800

    added two alternate solutions, which are waaaaay better. Will study those.

--- week03/betweenTwoArrays.js
+++ week03/betweenTwoArrays.js
@@ -72,3 +72,29 @@ const input = {
 };
 
 getTotalX(input.a8, input.b8);
+
+// alternate solution 1:
+// function getTotalX(a, b) {
+//   let count = 0;
+//   const max = Math.max(...b);
+	
+//   for (let i = 1; i <= max; i++) {
+// 	  if (a.every(num => i % num === 0) && b.every(num => num % i === 0)) count++;
+//   }
+	
+//   return count;	
+
+// alternate solution2:
+// function getTotalX(a, b) {
+//   const arr = [];
+//   while(arr.length < b[0]) arr.push(arr.length + 1)
+  
+//   let factors = 0;
+//   arr.forEach((e) => {
+//     const cond1 = a.every((el) => e % el === 0);
+//     const cond2 = b.every((el) => el % e === 0);
+//     if(cond1 && cond2) factors++;
+//   })
+  
+//   return factors;
+// }
