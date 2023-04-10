8f2c638321da5503be402e1bc5e8f76bd45a77d2
Todd Pangilinan <pang.todd@gmail.com>
2023-04-10 16:43:27 -0700

    established initial file and added prompt.

---
+++ week04/closestNo.js
@@ -0,0 +1,13 @@
+// promt:
+// Sorting is useful as the first step in many different tasks. The most common task is to make finding things easier, but there are other uses as well. In this case, it will make it easier to determine which pair or pairs of elements have the smallest absolute difference between them.
+
+// Example
+
+// arr = [5, 2, 3, 4, 1]
+
+// Sorted, arr= [1,2,3,4,5]. Several pairs have the minimum difference of 1: [(1,2),(2,3), (3,4), (4,5)]. Return the array [1,2,2,3,3,4,4,5].
+
+function closestNumbers(arr) {
+  // Write your code here
+
+}
