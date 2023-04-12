db8d79d43b12acf146b675fbdbc740201fa5fe7c
Todd Pangilinan <pang.todd@gmail.com>
2023-04-11 19:46:22 -0700

    established file/prompt

---
+++ week04/minAbsDistance.js
@@ -0,0 +1,6 @@
+// Prompt
+// The absolute difference is the positive difference between two values a  and b, is written |a-b| or |b-a|  and they are equal. If a=3 and b=2, |3-2|=|2-3|=1. Given an array of integers, find the minimum absolute difference between any two elements in the array.
+
+// Example: arr= [-2,2,4]
+
+// There are 3 pairs of numbers: [-2,-2], |[-2,4], [2,4] and . The absolute differences for these pairs are |(-2)-2|=4, (-2)-4|=6 and |2,4|=2. The minimum absolute difference is 2.
