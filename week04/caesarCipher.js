cd2ec0b676b4ba47e1c4f8b2c2d85e2207c7ccd3
Todd Pangilinan <pang.todd@gmail.com>
2023-04-12 23:25:04 -0700

    WIP, passes 5/12 test cases. Need to re-work how the key works- it is possible the rotation value exceeds the # of letters.

--- week04/caesarCipher.js
+++ week04/caesarCipher.js
@@ -1,2 +1,20 @@
 // prompt:
 // Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
+
+function caesarCipher(s, k) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const key = alphabet.substring(k)+alphabet.slice(0,k)
+  const upperCaseKey= key.toUpperCase();
+  let answer = "";
+  for(let i=0; i<s.length; i++){
+      let position = alphabet.indexOf(s[i].toLowerCase());
+      if(key.includes(s[i])){
+          answer = answer + key[position];          
+      }else if (upperCaseKey.includes(s[i])){
+          answer = answer + upperCaseKey[position];
+      } else {
+          answer = answer + s[i];
+      }
+  }
+  return answer;
+}
