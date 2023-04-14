9176dd8440f42efb67ff034aa8394f6a4d09fc2c
Todd Pangilinan <pang.todd@gmail.com>
2023-04-13 19:43:33 -0700

    establish file and prompt.

---
+++ week04/leftRotate.js
@@ -0,0 +1,7 @@
+// prompt:
+// A left rotation operation on an array of size n shifts each of the array's elements 1 unit to the left. Given an integer, d , rotate the array that many steps left and return the result.
+
+// Example
+// d =2 
+// arr =[1,2,3,4,5]
+// after 2 rotations arr=[3,4,5,1,2]
