0b53e4107974f8506e6e894802bb47798a1a97c6
Todd Pangilinan <pang.todd@gmail.com>
2023-04-18 19:45:29 -0700

    initial file and prompt added.

---
+++ week04/anagram.js
@@ -0,0 +1,6 @@
+prompt:
+2 words ar anagrams of one another if there letters can be rearranged to form the other word. Given the String, split it into two contiguous substrings of equal length. Determine the minimum number of characters to change to make the two substrings into anagrams of one another.
+
+example: s = abccde
+splits into "abc" and "cde"
+2 changes are required.
