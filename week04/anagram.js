f7cad23521751cb4ad426d420693351cabb2e1c7
Todd Pangilinan <pang.todd@gmail.com>
2023-04-18 21:49:22 -0700

    wrote passing solution.

--- week04/anagram.js
+++ week04/anagram.js
@@ -4,3 +4,34 @@ prompt:
 example: s = abccde
 splits into "abc" and "cde"
 2 changes are required.
+
+function anagram(s) {
+  let inCommon = 0;
+  if (s.length % 2 > 0) {
+    return -1
+  } else {
+    const subStringA = s.slice(0, s.length / 2);
+    let subStringB = s.slice(s.length / 2, );
+    for (let i = 0; i < s.length / 2; i++) {
+      if (subStringB.includes(subStringA[i])) {
+        inCommon = inCommon + 1;
+        subStringB = subStringB.replace(subStringA[i], "")
+      }
+    }
+    return s.length / 2 - inCommon
+  }
+}
+
+// test info:
+
+// aaabbb
+// ab
+// abc
+// mnop
+// xyyx
+// xaxbbbxx
+// hhpddlnnsjfoyxpciioigvjqzfbpllssuj
+// xulkowreuowzxgnhmiqekxhzistdocbnyozmnqthhpievvlj
+// dnqaurlplofnrtmh
+// aujteqimwfkjoqodgqaxbrkrwykpmuimqtgulojjwtukjiqrasqejbvfbixnchzsahpnyayutsgecwvcqngzoehrmeeqlgknnb
+// lbafwuoawkxydlfcbjjtxpzpchzrvbtievqbpedlqbktorypcjkzzkodrpvosqzxmpad
