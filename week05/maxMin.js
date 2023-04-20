4460c1ed43078a18556269c38c3a18b42e1a1714
Todd Pangilinan <pang.todd@gmail.com>
2023-04-20 15:31:38 -0700

    establish initial file and prompt.

---
+++ week05/maxMin.js
@@ -0,0 +1,8 @@
+<!-- prompt:
+You will be given a list of integers, arr , and a single integer k. You must create an array of length k from elements of arr such that its unfairness is minimized. Call that array arr'. Unfairness of an array is calculated as
+
+max(arr')-min(arr')
+
+Where:
+- max denotes the largest integer in 
+- min denotes the smallest integer in  -->
