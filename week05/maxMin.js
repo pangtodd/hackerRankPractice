719fb2ef66147560410d31f5c3a0a287fef5195d
Todd Pangilinan <pang.todd@gmail.com>
2023-04-22 18:07:11 -0700

    wrote passing code

--- week05/maxMin.js
+++ week05/maxMin.js
@@ -9,10 +9,9 @@
 
 function maxMin(k, arr) {
   const sorted = arr.sort((a,b)=>(a-b)).reverse();
-  console.log(sorted);
-  let minScore= sorted[0]-sorted[1]
+  let minScore= sorted[0]-sorted[k-1]
   for (let i=0; i<arr.length; i++){
-      let iDiff = sorted[i]-sorted[i+1]
+      let iDiff = sorted[i]-sorted[i+(k-1)]
      if(iDiff<minScore){
          minScore= iDiff
      }
