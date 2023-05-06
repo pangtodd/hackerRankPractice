6175411ae2774d66b8d585d0a55f3584f918b984
Todd Pangilinan <pang.todd@gmail.com>
2023-05-05 19:43:58 -0700

    added initial file and prompt.

---
+++ week05/fullCountingSort.js
@@ -0,0 +1,6 @@
+// prompt
+// Use the counting sort to order a list of strings associated with integers. If two strings are associated with the same integer, they must be printed in their original order, i.e. your sorting algorithm should be stable. There is one other twist: strings in the first half of the array are to be replaced with the character - (dash, ascii 45 decimal).
+
+// Insertion Sort and the simple version of Quicksort are stable, but the faster in-place version of Quicksort is not since it scrambles around elements while sorting.
+
+// Design your counting sort to be stable.
