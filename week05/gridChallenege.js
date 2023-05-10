d56053a790e483d491711033e33c32b8bb3e3214
Todd Pangilinan <pang.todd@gmail.com>
2023-05-10 16:57:01 -0700

    WIP, wrote solution that passes 7/13 tests. Refactoring.

--- week05/gridChallenege.js
+++ week05/gridChallenege.js
@@ -1,2 +1,17 @@
 // prompt:
 // Given a square grid of characters in the range ascii[a-z], rearrange elements of each row alphabetically, ascending. Determine if the columns are also in ascending alphabetical order, top to bottom. Return YES if they are or NO if they are not.
+
+function gridChallenge(grid) {
+  let answer = "";
+  for(let i=0; i<grid.length; i++){
+      let sorted = grid[i].split("").sort();
+      answer+=sorted[0]
+  }
+  console.log("answer: "+answer)
+  console.log("sorted answer: "+ answer.split("").sort().join(''))
+  if (answer.split("").sort().join('') === answer){
+      return "YES"
+  } else {
+      return "NO"
+  }
+}
