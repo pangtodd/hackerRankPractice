ef4adba456786242615fbd2346a85299eb1f35e9
Todd Pangilinan <pang.todd@gmail.com>
2023-05-16 21:01:22 -0700

    established initial file and prompt.

---
+++ week05/fibonacciModified.js
@@ -0,0 +1,19 @@
+prompt:
+implement a modified Fibonacci sequence using the follwing definition:
+
+given terms t[i] and t[i+1] where i (1, infinity), term t[i+2] is computed as:
+
+t i+2 = t i + (t i+1)squared
+
+Given three integers, t1, t2 and n, compute and print the nth term of a modified fibonacci sequecnce.
+
+Example:
+t1= 0
+t2 =1
+n = 6
+-t3 = 0 + 1squared = 1
+-t4 = 1 + 1squared = 2
+-t5 = 1 + 2squared = 5
+-t6 = 2 + 5squared = 27
+
+ 
