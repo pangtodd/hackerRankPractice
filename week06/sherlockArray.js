47e49100c6ff1690d6379aae956482e3eaa9848e
Todd Pangilinan <pang.todd@gmail.com>
2023-05-19 15:27:44 -0700

    establishe initial file, added prompt. Re-wrote examples for clairity.

---
+++ week06/sherlockArray.js
@@ -0,0 +1,15 @@
+// prompt:
+// Watson gives Sherlock an array of integers. His challenge is to find an element of the array such that the sum of all elements to the left is equal to the sum of all elements to the right.
+
+// Example
+
+// arr = [5, 6, 8, 11]
+
+// The answer is YES 8 is between two subarrays that sum to 11.
+
+// arr = [1]
+
+// The answer is YES since left and right sum to 0.
+
+
+// You will be given arrays of integers and must determine whether there is an element that meets the criterion. If there is, return YES. Otherwise, return NO.
