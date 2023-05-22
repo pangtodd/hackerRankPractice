543f34a73a643b0800582fd00386a67707446b86
Todd Pangilinan <pang.todd@gmail.com>
2023-05-21 17:38:24 -0700

    had to look up more about the problem, wasn't going to be able to figure it out of my own- required knowledge of game theory & XOR. added alternate solution.

--- week06/misereNim.js
+++ week06/misereNim.js
@@ -6,3 +6,13 @@
 // * The player who removes the last stone loses the game.
 
 // Given the value of n and the number of stones in each pile, determine whether the person who wins the game is the first or second person to move. If the first player to move wins, print First on a new line; otherwise, print Second. Assume both players move optimally.
+
+function misereNim(s) {
+  let sum = s.reduce((a,b) => a + b);
+  if(sum == s.length) {
+      return s.length % 2 == 0 ? 'First' : 'Second'
+  }
+
+   let nimSum = s.reduce((a,b) => a ^ b);
+   return nimSum == 0 ? 'Second' : 'First'
+}
