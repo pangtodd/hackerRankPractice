7e08943e2f7abf1fdb0e0c091b5292c1330b3437
Todd Pangilinan <pang.todd@gmail.com>
2023-05-24 18:41:51 -0700

    initial file and prompt added.

---
+++ week06/counterGame.js
@@ -0,0 +1,12 @@
+// prompt:
+// Louise and Richard have developed a numbers game. They pick a number and check to see if it is a power of 2. If it is, they divide it by 2. If not, they reduce it by the next lower number which is a power of 2. Whoever reduces the number to 1 wins the game. Louise always starts.
+
+// Given an initial value, determine who wins the game.2]
+
+// Example
+
+//  n = 132
+
+// It's Louise's turn first. She determines that 132 is not a power of 2. The next lower power of 2 is 128, so she subtracts that from 132  and passes 4 to Richard. 4  is a power of 2, so Richard divides it by 2 and passes 2 to Louise. Likewise, 2 is a power so she divides it by 2 and reaches 1. She wins the game.
+
+// Update If they initially set counter to 1, Richard wins. Louise cannot make a move so she loses.
