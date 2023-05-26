40c752aa9c018eb593ebac5277d89653fab41165
Todd Pangilinan <pang.todd@gmail.com>
2023-05-25 17:59:58 -0700

    WIP, first attempt only passes 5/21 test cases. Not sure Math.log2 is accurate enough when considering larger numbers.

--- week06/counterGame.js
+++ week06/counterGame.js
@@ -10,3 +10,18 @@
 // It's Louise's turn first. She determines that 132 is not a power of 2. The next lower power of 2 is 128, so she subtracts that from 132  and passes 4 to Richard. 4  is a power of 2, so Richard divides it by 2 and passes 2 to Louise. Likewise, 2 is a power so she divides it by 2 and reaches 1. She wins the game.
 
 // Update If they initially set counter to 1, Richard wins. Louise cannot make a move so she loses.
+
+function counterGame(n) {
+  let score = 0;
+  while (n>1){
+      let power = Math.floor(Math.log2(n));
+      score +=1;
+      Math.pow(2, power)==n? n=n/2: n=Math.pow(2, power);
+      console.log(n)    
+  }
+  if(score % 2== 0){
+      return "Louise"
+  } else {
+      return "Richard"
+  }
+} 
