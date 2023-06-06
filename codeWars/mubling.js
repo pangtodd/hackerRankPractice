107f7001a0ec301a44ea56253f1631bca1e02be7
Todd Pangilinan <pang.todd@gmail.com>
2023-06-06 16:33:42 -0700

    wrote passing (but ugly) solution.

--- codeWars/mubling.js
+++ codeWars/mubling.js
@@ -7,3 +7,11 @@
 // accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 // accum("cwAt") -> "C-Ww-Aaa-Tttt"
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+  let answer = s[0].toUpperCase();
+	for (let i = 1; i <s.length; i++){
+    answer += "-"+s[i].toUpperCase()+s[i].toLowerCase().repeat(i)
+  }
+  return answer
+}
