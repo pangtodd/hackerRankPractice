522f9a2e5ea0801b471969463611fd3d3cfd2607
Todd Pangilinan <pang.todd@gmail.com>
2023-06-05 20:27:50 -0700

    wrote passing function, but might refator as a ternary operator.

--- codeWars/banjo.js
+++ codeWars/banjo.js
@@ -11,6 +11,9 @@
 
 
 function areYouPlayingBanjo(name) {
-  
-  return name;
+  if(name[0] === "r"||name[0]==="R"){
+    return name+" plays banjo"
+  } else {
+    return name+ " does not play banjo"
+  }
 }
