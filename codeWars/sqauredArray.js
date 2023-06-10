44828c61e98d50fa54a2d0f98d8ff4c4589c92e0
Todd Pangilinan <pang.todd@gmail.com>
2023-06-09 19:50:08 -0700

    established file and prompt.

---
+++ codeWars/sqauredArray.js
@@ -0,0 +1,8 @@
+// prompt:
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 
+// 1 squared = 1
+// 2 squared = 4
+// 2 sqared = 4
+// summed = 9
