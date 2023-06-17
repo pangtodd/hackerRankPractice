425f6980aebb05eaab6152363831132212100a1b
Todd Pangilinan <pang.todd@gmail.com>
2023-06-17 16:43:41 -0700

    wrote passing function.

--- codeWars/uniqueFilter.js
+++ codeWars/uniqueFilter.js
@@ -1,9 +1,19 @@
-Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
 
-Examples:
-a = "xyaabbbccccdefww"
-b = "xxxxyyyyabklmopq"
-longest(a, b) -> "abcdefklmopqwxy"
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
 
-a = "abcdefghijklmnopqrstuvwxyz"
-longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+  const combo= s1+s2;
+  let answer= ""
+  for (let i= 0; i <combo.length; i++)
+ 		if ( answer.includes(combo[i])==false){
+    	answer +=combo[i]
+    }
+  return answer.split("").sort().join("")
+}
