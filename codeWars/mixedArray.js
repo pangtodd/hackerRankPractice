e585a95d9e31ac6e68837fc74c20b65c313fe74a
Todd Pangilinan <pang.todd@gmail.com>
2023-06-18 10:52:56 -0700

    added alt solution, which uses a loop, other solutions were similar to mine.

--- codeWars/mixedArray.js
+++ codeWars/mixedArray.js
@@ -6,3 +6,12 @@
 function sumMix(x){
   return  x.map(number=>parseInt(number)).reduce((a,b)=>a+b)
  }
+
+//  alt solution:
+//  function sumMix(x){
+//   let result = 0;
+//   for (let n of x) {
+//     result += parseInt(n);
+//   }
+//   return result;
+// }
