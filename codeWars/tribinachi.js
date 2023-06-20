fdb5d7df67f1c4d5d5629ff00eed1527a6cd3ee1
Todd Pangilinan <pang.todd@gmail.com>
2023-06-19 19:40:38 -0700

    added alt solution, similar logic, but used a while loop with reduce.

--- codeWars/tribinachi.js
+++ codeWars/tribinachi.js
@@ -19,3 +19,12 @@ function tribonacci(signature,n){
   }
   return signature.slice(0,n)
 }
+
+// alt soltuion:
+// function tribonacci(signature,n) {
+//   const result = signature.slice(0, n);
+//   while (result.length < n) {
+//     result[result.length] = result.slice(-3).reduce((p,c) => p + c, 0);
+//   }
+//   return result;
+// }
