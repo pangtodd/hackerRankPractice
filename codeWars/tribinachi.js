61b163d974d738e3866fcf05c727f20a6f3d4596
Todd Pangilinan <pang.todd@gmail.com>
2023-06-19 19:38:02 -0700

    wrote passing solution.

--- codeWars/tribinachi.js
+++ codeWars/tribinachi.js
@@ -12,3 +12,10 @@
 // Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
 
 // Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+
+function tribonacci(signature,n){
+  for(let i=0;i<n-3;i++){
+    signature.push(signature[i]+signature[i+1]+signature[i+2])
+  }
+  return signature.slice(0,n)
+}
