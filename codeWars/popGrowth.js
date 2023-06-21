38e2aa9325276295cda3d6dbbee1049f8f562d4d
Todd Pangilinan <pang.todd@gmail.com>
2023-06-20 20:12:39 -0700

    added alt solution, recursive, but kinda similar logic.

--- codeWars/popGrowth.js
+++ codeWars/popGrowth.js
@@ -34,3 +34,11 @@ function nbYear(p0, percent, aug, p) {
     }
   return score
 }
+
+// alt solution:
+// function nbYear(p0, percent, aug, p) {
+//   p0 =  Math.floor(p0*(1+percent/100)+aug)
+//   if (p0>=p)
+//      return 1
+//   return nbYear(p0,percent,aug,p)+1
+// }
