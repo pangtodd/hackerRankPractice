bdfa716827d8b5a2ada123b9131b077df0583de1
Todd Pangilinan <pang.todd@gmail.com>
2023-06-21 23:39:58 -0700

    wrote passing solution.

--- codeWars/likes.js
+++ codeWars/likes.js
@@ -9,3 +9,16 @@
 // ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
 // ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 // Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+function likes(names) {
+  if (names.length < 1) {
+    return 'no one likes this';
+  } else if (names.length === 1) {
+    return names[0] + " likes this";
+  } else if (names.length < 4) {
+    const last = names.pop();
+    return `${names.join(', ')} and ${last} like this`
+  } else {
+    return `${names[0]}, ${names[1] } and ${names.length -2} others like this`
+  }
+}
