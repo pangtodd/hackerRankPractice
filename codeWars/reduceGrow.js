de7dc89ed8fa154917a207cd386f529447493faa
Todd Pangilinan <pang.todd@gmail.com>
2023-06-22 16:14:29 -0700

    added alt solution. Most were similar to mine, this was the only looped solution.

--- codeWars/reduceGrow.js
+++ codeWars/reduceGrow.js
@@ -6,3 +6,12 @@
 function grow(x){
   return x.reduce((a,b)=>a*b)
  }
+
+//  alt soltuion:
+//  const grow = x => {
+//   let res = 1;
+//   for (let i = 0; i < x.length; i++) {
+//     res *= x[i];
+//   }
+//   return res;
+// };
