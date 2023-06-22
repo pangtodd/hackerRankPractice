dd3bf76e3cdc507164057b53d2e31a6ab10b2c12
Todd Pangilinan <pang.todd@gmail.com>
2023-06-21 23:43:18 -0700

    added alt soltuion, which uses case statement.

--- codeWars/likes.js
+++ codeWars/likes.js
@@ -22,3 +22,15 @@ function likes(names) {
     return `${names[0]}, ${names[1] } and ${names.length -2} others like this`
   }
 }
+
+// alt solution:
+// function likes(names) {
+//   names.length === 0 && (names = ["no one"]);
+//   let [a, b, c, ...others] = names;
+//   switch (names.length) {
+//     case 1: return `${a} likes this`;
+//     case 2: return `${a} and ${b} like this`;
+//     case 3: return `${a}, ${b} and ${c} like this`;
+//     default: return `${a}, ${b} and ${others.length + 1} others like this`;
+//   }
+// }
