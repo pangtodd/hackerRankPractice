6b3b042c693445e3463a70e31de412bf9f99260e
Todd Pangilinan <pang.todd@gmail.com>
2023-06-23 20:24:22 -0700

    wrote a passing (but ugly/long) solution.

--- codeWars/smilelyCount.js
+++ codeWars/smilelyCount.js
@@ -16,3 +16,22 @@
 // countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
 // Note
 // In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+
+function countSmileys(arr) {
+  let score = 0;
+  const eyes = ":;"
+  const nose = "-~"
+  const mouth = "D)"
+  for (let i = 0; i < arr.length; i++) {
+  	let smile = "";
+    let thisSmile = arr[i];
+    smile += eyes.includes(thisSmile[0]) ? "t":"f";
+    smile += mouth.includes(thisSmile[thisSmile.length-1])? "t": "f"
+   	if(arr[i].length>2){
+    	smile += nose.includes(thisSmile[1])? "t":"f"
+    }
+    smile += arr[i].length>3? "f":"t"
+    score += smile.includes("f") ? 0 : 1
+  }
+  return score;
+}
