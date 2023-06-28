a5fb57243e30e5407ac5df4f1a54b520001246f9
Todd Pangilinan <pang.todd@gmail.com>
2023-06-27 17:51:25 -0700

    wrote passing solution.

--- codeWars/nthTerm.js
+++ codeWars/nthTerm.js
@@ -14,3 +14,13 @@
 // 1 --> 1 --> "1.00"
 // 2 --> 1 + 1/4 --> "1.25"
 // 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+function SeriesSum(n) {
+  let sum = 0;
+  let current = 1
+  for(let i = 0; i < n; i++) {
+    sum += 1/current;
+    current +=3;
+  }
+  return sum.toFixed(2);
+}
