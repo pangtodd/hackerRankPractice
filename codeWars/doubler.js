932e05924399b678535e15cfec7f5fbe8cc9afa1
Todd Pangilinan <pang.todd@gmail.com>
2023-06-28 21:29:13 -0700

    added alt solution which was a basic loop. All other solutions utilized map.

--- codeWars/doubler.js
+++ codeWars/doubler.js
@@ -8,3 +8,13 @@
 function maps(x){
   return x.map(y=>y*2)
 }
+
+// alt solution:
+// function maps(x){
+// //return x.map(el => el * 2);
+// let arr = [];
+// for(let i = 0; i < x.length; i++){
+// arr.push(x[i] * 2);
+// }
+// return arr;
+// }
