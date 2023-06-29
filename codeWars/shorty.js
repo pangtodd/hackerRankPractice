5b8fae3fe91b50e9f2aea9a6b30bfbd9628a0258
Todd Pangilinan <pang.todd@gmail.com>
2023-06-28 21:24:03 -0700

    added alt solution, which used Math.min instead of sort- similar logic otherwise.

--- codeWars/shorty.js
+++ codeWars/shorty.js
@@ -6,3 +6,8 @@
 function findShort(s){
   return s.split(" ").map(x=> x = x.length).sort((a,b)=>a-b)[0]
 }
+
+// alt solution:
+// function findShort(s){
+//   return Math.min(...s.split(" ").map (s => s.length));
+// }
