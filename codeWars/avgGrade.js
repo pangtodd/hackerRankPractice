7bd31350e1b195cc2c6699788ed30ca45a232f69
Todd Pangilinan <pang.todd@gmail.com>
2023-06-29 17:43:36 -0700

    added alt solution, which uses a basic for loop- all other solutions used reduce/similar to mine.

--- codeWars/avgGrade.js
+++ codeWars/avgGrade.js
@@ -12,3 +12,17 @@
 function betterThanAverage(classPoints, yourPoints) {
   return classPoints.reduce((a,b)=>a+b)/classPoints.length > yourPoints? false : true
 }
+
+// alt solution:
+// function betterThanAverage(classPoints, yourPoints) {
+//   var sum = 0;
+//   for (var i = 0; i < classPoints.length; i++){
+//   	sum += classPoints[i];
+//   }
+//   	sum = sum/classPoints.length
+//   if(sum > yourPoints) {
+//   	return false
+//   } else {
+//   	return true
+//   } 
+// }
