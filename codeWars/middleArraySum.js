da9930d8a9945b886912320788f9eab3002c214a
Todd Pangilinan <pang.todd@gmail.com>
2023-06-30 22:52:00 -0700

    added alt solution, similar, but uses a for loop instead of reduce. Most other solutuions used reduce.

--- codeWars/middleArraySum.js
+++ codeWars/middleArraySum.js
@@ -17,3 +17,18 @@ function sumArray(array) {
   else
     return array.sort((a,b)=>a-b).slice(1, -1).reduce((a,b)=>a+b,0)
 }
+
+// function sumArray(array) {
+//   if (array == null) {
+//     return 0;
+//   } else if (array.length < 2) {
+//     return 0;
+//   } else {
+//     array = array.sort(function(a,b) {return a - b;});
+//     var total = 0;
+//     for (var i = 1; i < array.length - 1; i++) {
+//       total += array[i];
+//     }
+//     return total;
+//   }
+// }
