626284c36ec27cc4c29efd5d7bda58230e3e7b5f
Todd Pangilinan <pang.todd@gmail.com>
2023-07-04 16:16:02 -0700

    WIP, wrote a solution that passed the initial test, but failed a larger array. Refactoring.

--- codeWars/limitList.js
+++ codeWars/limitList.js
@@ -8,3 +8,14 @@
 // For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
 // With list [20,37,20,21] and number 1, the result would be [20,37,21].
 
+function deleteNth(arr, n) {
+	let count = 0;
+  let newArr=arr;
+  for (let i = 0; i < arr.length; i++) {
+    count = newArr.filter(x => x == newArr[i]).length;
+    if (count > n) {
+			newArr.splice(newArr.lastIndexOf(newArr[i]), 1);
+    }
+  }
+  return arr
+}
