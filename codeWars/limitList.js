b70fe58f704c09172609bb20f42086fb027eae22
Todd Pangilinan <pang.todd@gmail.com>
2023-07-05 07:40:00 -0700

    added an alt solution from another participant, which is much more performant, using an object to count/filter the instances.

--- codeWars/limitList.js
+++ codeWars/limitList.js
@@ -20,3 +20,11 @@ function deleteNth(arr, n) {
       	while(count>n);
     }
   }
+
+  // function deleteNth(arr,x) {
+  //   var cache = {};
+  //   return arr.filter(function(n) {
+  //     cache[n] = (cache[n]||0) + 1;
+  //     return cache[n] <= x;
+  //   });
+  // }
