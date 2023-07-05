84cd03a47319120f6f8bf09babae119cb4f217b6
Todd Pangilinan <pang.todd@gmail.com>
2023-07-05 07:56:03 -0700

    added alt solution, which sorts the array and then returns the first and last position of the array. I considered this, but it seems like it would be less efficent to sort the array vs. using min/max.

--- codeWars/highLow.js
+++ codeWars/highLow.js
@@ -14,3 +14,9 @@
 function minMax(arr){
   return [Math.min(...arr), Math.max(...arr)]; 
 }
+
+// alt:
+// function minMax(arr) {
+//   var a = arr.sort(function(a,b) {return a-b});
+//   return [a[0], a[a.length-1]];
+// }
