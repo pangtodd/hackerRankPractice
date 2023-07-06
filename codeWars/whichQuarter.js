687fe730a51a7d982da17fb9d27e7d0a471861b6
Todd Pangilinan <pang.todd@gmail.com>
2023-07-05 17:39:14 -0700

    wrote passing solution with a simple if/else.

--- codeWars/whichQuarter.js
+++ codeWars/whichQuarter.js
@@ -6,3 +6,15 @@
 // Constraint:
 
 // 1 <= month <= 12
+
+const quarterOf = (month) => {
+  if(month<4){
+    return 1
+  } else if (month<7){
+    return 2
+  } else if (month<10){
+    return 3
+  } else{
+    return 4
+  } 
+}
