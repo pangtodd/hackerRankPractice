f36de29d96de5f08784bf84a865778d44597e0e8
Todd Pangilinan <pang.todd@gmail.com>
2023-07-07 16:53:35 -0700

    wrote passing solution using filter and Number.isInteger.

--- codeWars/NumsOnly.js
+++ codeWars/NumsOnly.js
@@ -5,3 +5,7 @@
 // filter_list([1,2,'a','b']) == [1,2]
 // filter_list([1,'a','b',0,15]) == [1,0,15]
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+function filter_list(l) {
+  return l.filter(e=> Number.isInteger(e)== true)
+}
