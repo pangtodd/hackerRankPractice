ecc53b07b190609e273e50a9c67aed97a5e347b1
Todd Pangilinan <pang.todd@gmail.com>
2023-07-07 16:55:17 -0700

    added alt solution, also used filter, but used typeof instead of isInteger.

--- codeWars/NumsOnly.js
+++ codeWars/NumsOnly.js
@@ -9,3 +9,8 @@
 function filter_list(l) {
   return l.filter(e=> Number.isInteger(e)== true)
 }
+
+// alt solution:
+// function filter_list(l) {
+//   return l.filter(function(v) {return typeof v == 'number'})
+// }
