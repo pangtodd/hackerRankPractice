5a68d9d5da8dbabee6adcb07fc6d24447fcc7bed
Todd Pangilinan <pang.todd@gmail.com>
2023-07-08 11:58:54 -0700

    added alt solution with similar logic, but used a chained ternary operator, which I didn't consider (and is more concise)

--- codeWars/easyA.js
+++ codeWars/easyA.js
@@ -24,3 +24,9 @@ function getGrade(s1, s2, s3) {
     return "F";
   }
 }
+
+// alt solution:
+// function getGrade (s1, s2, s3) {
+//   var s = (s1 + s2 + s3) / 3
+//   return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F"
+// }
