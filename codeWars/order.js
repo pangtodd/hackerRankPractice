6008ae7f78caa478b2330469aa9ddd21b45ddb28
Todd Pangilinan <pang.todd@gmail.com>
2023-07-10 19:53:37 -0700

    wrote passing solution, used a for loop and filter to find the numeric value in each word, and then pushed it into a new array in the correct order.

--- codeWars/order.js
+++ codeWars/order.js
@@ -9,3 +9,12 @@
 // "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
 // "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
 // ""  -->  ""
+
+function order(words){
+  let answer = []
+  const wordsArr = words.split(" ")
+  for (let i=1; i<wordsArr.length+1; i++){
+    answer.push(wordsArr.filter(e=> e.includes(i)))
+  }
+  return answer.join(" ")
+}
