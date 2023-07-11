939696aa7ef297eb05d54025045e096aa0ef183c
Todd Pangilinan <pang.todd@gmail.com>
2023-07-10 17:24:43 -0700

    added alt solution- almost all were similar to mine, but this solution went ahead and set variables for area and perimeter before the ternary. Same logic, just a little different execution.

--- codeWars/areaVsPerimeter.js
+++ codeWars/areaVsPerimeter.js
@@ -11,3 +11,11 @@
 const areaOrPerimeter = function(l , w) {
   return l === w ? l*w : 2 * (l+w)
 };
+
+// alt solution:
+// const areaOrPerimeter = function(l , w) {
+//   let area = l * w;
+//   let perimeter = (l + w) * 2;
+  
+//   return l === w ? area : perimeter;
+// };
