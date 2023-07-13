95d5d4d372aed0fc359c0d9bafc93e20d8e7b1d6
Todd Pangilinan <pang.todd@gmail.com>
2023-07-13 16:37:12 -0700

    added alt solution, which was sort of similar, but used substr w/Math.ceil, and also placed the ternary within the argument for Math.ceil (hadn't considered that before).

--- codeWars/midChar.js
+++ codeWars/midChar.js
@@ -20,3 +20,9 @@
 function getMiddle(s){
   return s.length%2===0? s.slice(s.length/2-1, s.length/2+1): s.slice(s.length/2,s.length/2+1);
 }
+
+// alt solution:
+// function getMiddle(s)
+// {
+//   return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+// }
