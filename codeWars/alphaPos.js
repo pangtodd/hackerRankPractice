b95159d85d66ed40f33434d69c3902869c1b265b
Todd Pangilinan <pang.todd@gmail.com>
2023-07-12 20:01:59 -0700

    added TWO alt solutions- the first one is a better version of what I was trying to do- no loops, just uses filter and map. The second alt solution uses charCodeAt, and subtracts 64 to get the correct position. Clever, hadn't considered that.

--- codeWars/alphaPos.js
+++ codeWars/alphaPos.js
@@ -25,3 +25,26 @@ function alphabetPosition(text) {
   }
   return newText.replaceAll(" ", "").split(",").filter(e=> parseInt(e)>= 0).join(" ")
 }
+
+// alt soltuion 1:
+// function alphabetPosition(text) {
+//   var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+//   return text.toLowerCase().split('')
+//   .filter( letter => {
+//     let index = alphabet.indexOf(letter);
+//     return index > -1;
+//   }  )
+//   .map( letter => alphabet.indexOf(letter) + 1 )
+//   .join(' ')
+// }
+
+// alt solution 2:
+// function alphabetPosition(text) {
+//   var result = "";
+//   for (var i = 0; i < text.length; i++){
+//     var code = text.toUpperCase().charCodeAt(i)
+//     if (code > 64 && code < 91) result += (code - 64) + " ";
+//   }
+
+//   return result.slice(0, result.length-1);
+// }
