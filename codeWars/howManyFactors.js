a75b1c1f538afbe735e96c4a320d3e5aa80357aa
Todd Pangilinan <pang.todd@gmail.com>
2023-07-22 14:02:36 -0700

    added alt solution, which was a recursive solution.

--- codeWars/howManyFactors.js
+++ codeWars/howManyFactors.js
@@ -20,3 +20,37 @@ function getDivisorsCnt(n){
   const fAnswer= [...new Set(answer)]
   return fAnswer.length
 }
+
+// alt solution:
+// function getDivisorsCnt(n) {  
+//   if (n === 1) return n
+
+//   // Use formuls http://vk.cc/4G1m8t
+//   let divArr = [[2, 0]] // 'divisor', 'number'
+//   let div = divArr[0][0] // first divisor
+
+//   while (n > 1) {
+//     if (n % div === 0) {
+//       for (let i = 0; divArr.length; i++) {
+//         if (divArr[i][0] === div) {
+//           divArr[i][1] += 1
+//           break
+//         } else {
+//           if (i === divArr.length - 1) {
+//             divArr.push([div, 1])
+//             break
+//           }
+//         }
+//       }
+//       n /= div
+//     } else {
+//       div += 1
+//     }
+//   }
+  
+//   for (let i = 0; i < divArr.length; i++) {
+//     n *= divArr[i][1] + 1
+//   }
+  
+//   return n
+// }
