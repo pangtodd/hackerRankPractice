806016eea0dab9049d7039dad89fe6e0250dbbe7
Todd Pangilinan <pang.todd@gmail.com>
2023-07-23 11:41:24 -0700

    added alt solution, which has similar logic, but is written much more concisely using a while loop.

--- codeWars/bounce.js
+++ codeWars/bounce.js
@@ -36,3 +36,10 @@ function bouncingBall(h, bounce, window) {
   while (h > window)
   return score
 }
+
+// alt solution:
+// function bouncingBall(h,  bounce,  window) {
+//   var rebounds = -1;
+//   if (bounce > 0 && bounce < 1) while (h > window) rebounds+=2, h *= bounce;
+//   return rebounds;
+// }
