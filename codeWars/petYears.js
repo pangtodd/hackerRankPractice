921a17a325f784cdf89560e37c259ab78e447de1
Todd Pangilinan <pang.todd@gmail.com>
2023-07-23 09:42:42 -0700

    added alt solution, similar logic but used case switch. Other solutions were similar.

--- codeWars/petYears.js
+++ codeWars/petYears.js
@@ -39,3 +39,15 @@ var humanYearsCatYearsDogYears = function(humanYears) {
   return [humanYears,catYears,dogYears];
 }
 
+// alt solution:
+// function humanYearsCatYearsDogYears(humanYears) {
+//   switch (humanYears) {
+//     case 1:
+//       return [1, 15, 15]
+//     case 2:
+//       return [2, 24, 24];
+//     default:
+//       return [humanYears, (humanYears - 2) * 4 + 24, (humanYears - 2) * 5 + 24 ];
+//   }
+  
+// }
