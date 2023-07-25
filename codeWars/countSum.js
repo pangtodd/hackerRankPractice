e960ebea280dcc2516c9facc65cadf2f69b42798
Todd Pangilinan <pang.todd@gmail.com>
2023-07-25 08:27:10 -0700

    wrote passing solution using filter and reduce. Got a little ugly b/c I had to account for some edge cases (if input was null, or if there were only positive numbers within an array, etc).

--- codeWars/countSum.js
+++ codeWars/countSum.js
@@ -8,3 +8,8 @@
 // Example
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
+function countPositivesSumNegatives(input) {
+  if(input == null || input.length ==0) return [];
+  const neg = input.filter(e=> e < 1);
+ 	return [input.length - neg.length, neg.length==0? 0: neg.reduce((a,b)=>a+b)]
+}
