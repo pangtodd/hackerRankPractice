511395526af9a2c3f935afb91b701e06e9d89981
Todd Pangilinan <pang.todd@gmail.com>
2023-07-24 20:29:16 -0700

    added alt solution- I didn't need to have a ternary operator with true:false, just needed to put the statement up and it would return the true or false.

--- codeWars/factorCheck.js
+++ codeWars/factorCheck.js
@@ -13,3 +13,8 @@
 function checkForFactor (base, factor) {
   return base%factor === 0? true :false
 }
+
+// alt solution:
+// function checkForFactor (base, factor) {
+//   return base % factor === 0;
+// }
