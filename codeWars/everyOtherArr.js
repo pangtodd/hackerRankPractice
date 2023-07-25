ce07960609242f259c73775c6d3f147a68723720
Todd Pangilinan <pang.todd@gmail.com>
2023-07-25 07:45:20 -0700

    wrote passing solution using a for loop and a modulo to only push the even elements into a new array.

--- codeWars/everyOtherArr.js
+++ codeWars/everyOtherArr.js
@@ -5,3 +5,13 @@
 // ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
 
 // None of the arrays will be empty, so you don't have to worry about that!
+
+function removeEveryOther(arr){
+  let answer = []
+  for(let i =0; i<arr.length; i++){
+    if (i % 2==0) {
+      answer.push(arr[i])
+    }
+  }
+  return answer
+}
