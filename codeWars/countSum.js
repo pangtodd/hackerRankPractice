54094f4d40c42c167db646a828e278fe254cd4cf
Todd Pangilinan <pang.todd@gmail.com>
2023-07-25 08:30:28 -0700

    added alt solution, similar logic, but uses a for loop to count/sum elements.

--- codeWars/countSum.js
+++ codeWars/countSum.js
@@ -13,3 +13,22 @@ function countPositivesSumNegatives(input) {
   const neg = input.filter(e=> e < 1);
  	return [input.length - neg.length, neg.length==0? 0: neg.reduce((a,b)=>a+b)]
 }
+
+// alt solution:
+// function countPositivesSumNegatives(input) {
+//   if (input == null || input.length == 0)
+//     return [];
+  
+//   var positive = 0;
+//   var negative = 0;
+  
+//   for (var i=0, l=input.length; i<l; ++i)
+//   {
+//     if (input[i] > 0)
+//       ++ positive;
+//     else
+//       negative += input[i];
+//   }
+  
+//   return [positive, negative];
+// }
