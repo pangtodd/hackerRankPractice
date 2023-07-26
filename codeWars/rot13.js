94bf453489f30498f0665b948f24a63b96178975
Todd Pangilinan <pang.todd@gmail.com>
2023-07-26 07:44:28 -0700

    wrote passing solution with a basic loop and indexOf.

--- codeWars/rot13.js
+++ codeWars/rot13.js
@@ -3,3 +3,11 @@
 
 // Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
 
+function rot13(message){
+  const alpha = "abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM";
+  let answer =""
+  for (let i = 0; i <message.length; i++){
+    alpha.includes(message[i]) ? answer+= alpha[alpha.indexOf(message[i])+13]: answer += message[i]
+  }
+  return answer
+}
