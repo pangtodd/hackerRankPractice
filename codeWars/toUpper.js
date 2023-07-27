a1ade7c6539a2eff2eb12ac343bef4120cbaf664
Todd Pangilinan <pang.todd@gmail.com>
2023-07-27 07:34:02 -0700

    added alt soltuion, which uses charCodeAt. all other solutions similar to mine.

--- codeWars/toUpper.js
+++ codeWars/toUpper.js
@@ -4,3 +4,17 @@
 function makeUpperCase(str) {
   return str.toUpperCase()
 }
+
+// alt solution:
+// function makeUpperCase(str) {
+//   let output = '';
+
+//   for (let i = 0; i < str.length; i++) {
+//     const charCode = str[i].charCodeAt(0);
+//     const chr = (charCode >= 97 && charCode <= 122) ? String.fromCharCode(charCode - 32) : str[i];
+
+//     output += chr;
+//   }
+  
+//   return output;
+// }
