187a04b68accaaca01c3bbf3c8097971794494ca
Todd Pangilinan <pang.todd@gmail.com>
2023-07-26 19:57:46 -0700

    added alt solution, actually pretty similar to mine, but the syntax is cleaner.

--- codeWars/consecutivePowers.js
+++ codeWars/consecutivePowers.js
@@ -83,3 +83,16 @@ function sumDigPow(a, b) {
   }
   return answer
 }
+
+// alt solution:
+// function sumDigPow(a, b) {
+//   var arr = [];
+//   for (var i = a; i <= b; i++) {
+//     var sum = 0;
+//     for (var j = 0; j <= String(i).length; j++) {
+//       sum += Math.pow(parseInt(String(i)[j]), j+1);  
+//       if (sum == i) arr.push(i);
+//     }
+//   }
+//   return arr;
+// }
