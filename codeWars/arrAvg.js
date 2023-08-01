26dce72940c6bd0799c3485cb72ae9de0ecd46ee
Todd Pangilinan <pang.todd@gmail.com>
2023-08-01 12:23:00 -0700

    wrote passing solution, using reduce/array.length and a ternary.

--- codeWars/arrAvg.js
+++ codeWars/arrAvg.js
@@ -2,3 +2,8 @@
 // Write a function which calculates the average of the numbers in a given list.
 
 // Note: Empty arrays should return 0.
+
+
+function findAverage(array) {
+  return array.length == 0? 0: array.reduce((a,b)=>a+b)/array.length
+}
