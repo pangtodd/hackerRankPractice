71b86921c80b50693196ff9f454570fc4f601525
Todd Pangilinan <pang.todd@gmail.com>
2023-08-02 14:35:32 -0700

    added alt solution, which use a similar loop, but used split to count the occurances. Clever use of split, hadn't considered that before.

--- codeWars/countDigit.js
+++ codeWars/countDigit.js
@@ -31,3 +31,12 @@ function nbDig(n, d) {
       }
   return huge.replaceAll(regex,"").length
 }
+
+alt solution:
+function nbDig(n, d) {
+  var o = '';
+    for(var i = 0; i <= n; i++){
+      o += Math.pow(i, 2);
+    }
+  return o.split(d).length-1
+}
