98ad26b992f0df9b9c753f9891f17e8c22600995
Todd Pangilinan <pang.todd@gmail.com>
2023-08-05 00:00:25 -0700

    added alt solution, used a loop.

--- codeWars/stringLengthSort.js
+++ codeWars/stringLengthSort.js
@@ -14,3 +14,23 @@
 function sortByLength (array) {
   return array.sort((a,b)=>a.length-b.length)
 };
+
+function compareNumeric(a, b) {
+	if (a < b) return -1;
+	if (a > b) return 1;
+}
+
+// function sortByLength (array) {
+// 	let arrayL = array.map((value) => {
+// 		return value.length;
+// 	})
+// 	arrayL = arrayL.sort(compareNumeric);
+// 	let resArr = arrayL.map((value) => {
+// 		for (let i = 0; i < array.length; i++) {
+// 			if (array[i].length === value) {
+// 				return array[i];
+// 			}
+// 		}
+// 	})
+// 	return resArr;
+// };
