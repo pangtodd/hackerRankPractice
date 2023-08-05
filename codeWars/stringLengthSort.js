3feb7129a14b7770c775a4ef5184b7bf034f1f77
Todd Pangilinan <pang.todd@gmail.com>
2023-08-04 23:59:14 -0700

    wrote passing solution using sort.

--- codeWars/stringLengthSort.js
+++ codeWars/stringLengthSort.js
@@ -10,3 +10,7 @@
 // ["Eyes", "Glasses", "Monocles", "Telescopes"]
 
 // All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+function sortByLength (array) {
+  return array.sort((a,b)=>a.length-b.length)
+};
