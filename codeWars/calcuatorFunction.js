b2b6f88849ed9356ec9511f42c47a2468fa324c3
Todd Pangilinan <pang.todd@gmail.com>
2023-08-06 11:51:34 -0700

    added alt solution, which cleverly uses spread operator, map...I wouldn't have been able to come up with this.

--- codeWars/calcuatorFunction.js
+++ codeWars/calcuatorFunction.js
@@ -28,3 +28,7 @@ function plus(right) {return (left) => left + right;}
 function minus(right) {return (left) => left -right;}
 function times(right) {return (left) => left *right;}
 function dividedBy(right) {return (left) => Math.floor(left/right);}
+
+// alt solution:
+// const [zero, one, two, three, four, five, six, seven, eight, nine] = [...Array(10)].map((_, idx) => fn => fn ? fn(idx) : idx);
+// const [plus, minus, times, dividedBy] = [`+`, `-`, `*`, `/`].map(val => new Function(`b`, `return a => a ${val} b ^ 0`));
