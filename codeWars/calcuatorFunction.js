0b4c8518be747cc8733811381becaa7d6b64b512
Todd Pangilinan <pang.todd@gmail.com>
2023-08-06 11:33:24 -0700

    after research and ChatGPT help, I wrote a passing solution using a ternary.

--- codeWars/calcuatorFunction.js
+++ codeWars/calcuatorFunction.js
@@ -12,3 +12,19 @@
 // Each calculation consist of exactly one operation and two numbers
 // The most outer function represents the left operand, the most inner function represents the right operand
 // Division should be integer division. For example, this should return 2, not 2.666666...:
+
+function zero(mathy) {return mathy?mathy(0):0}
+function one(mathy) {return mathy?mathy(1):1}
+function two(mathy) {return mathy?mathy(2):2}
+function three(mathy) {return mathy?mathy(3):3}
+function four(mathy) {return mathy?mathy(4):4}
+function five(mathy) {return mathy?mathy(5):5}
+function six(mathy) {return mathy?mathy(6):6}
+function seven(mathy){return mathy?mathy(7):7}
+function eight(mathy) {return mathy?mathy(8):8}
+function nine(mathy) {return mathy?mathy(9):9}
+
+function plus(right) {return (left) => left + right;}
+function minus(right) {return (left) => left -right;}
+function times(right) {return (left) => left *right;}
+function dividedBy(right) {return (left) => Math.floor(left/right);}
