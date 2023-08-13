613d74fb0af022767169576ccdbe6b46cf3a4978
Todd Pangilinan <pang.todd@gmail.com>
2023-08-12 20:08:24 -0700

    added alt solution, which uses map instead of a loop, but other logice pretty similar.

--- codeWars/jadenCasing.js
+++ codeWars/jadenCasing.js
@@ -18,3 +18,10 @@ String.prototype.toJadenCase = function () {
   }
 return answer.join(" ")
 };
+
+// alt solution:
+// String.prototype.toJadenCase = function () { 
+//   return this.split(" ").map(function(word){
+//     return word.charAt(0).toUpperCase() + word.slice(1);
+//   }).join(" ");
+// }
