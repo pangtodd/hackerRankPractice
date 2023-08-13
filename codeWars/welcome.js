0b6261864dff88ac7963e30ee187f8117391219c
Todd Pangilinan <pang.todd@gmail.com>
2023-08-13 10:15:13 -0700

    wrote passing solution by creating an object of languages/hello

--- codeWars/welcome.js
+++ codeWars/welcome.js
@@ -28,3 +28,29 @@
 // IP_ADDRESS_NOT_FOUND - ip address not in the database
 // IP_ADDRESS_REQUIRED - no ip address was supplied
 // FUNDAMENTALS
+
+function greet(language) {
+  const greeting = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }
+  return Object.keys(greeting).includes(language) ?
+    greeting[language]
+    :
+    greeting.english
+}
