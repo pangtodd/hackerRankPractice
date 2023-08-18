e07eac7465ffcd859f916cc3479b90ce19990cea
Todd Pangilinan <pang.todd@gmail.com>
2023-08-17 17:33:49 -0700

    wrote passing solution using while loop, toString, split and reduce.

--- codeWars/persistence.js
+++ codeWars/persistence.js
@@ -6,3 +6,14 @@
 // 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
 // 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
 // 4 --> 0 (because 4 is already a one-digit number)
+
+
+function persistence(num) {
+  let calc = num;
+  let answer = 0
+  while (calc>9){
+    calc = calc.toString().split("").reduce((a,b)=>a*b)
+    answer +=1
+  } 
+  return answer
+}
