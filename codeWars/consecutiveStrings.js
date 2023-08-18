fef9f04f6f6386ad668b4709456e9d55db05e533
Todd Pangilinan <pang.todd@gmail.com>
2023-08-18 10:32:22 -0700

    added alt solution, which used map instead of a loop, and reduce to return the largest-length word, which I hadn't considered.

--- codeWars/consecutiveStrings.js
+++ codeWars/consecutiveStrings.js
@@ -38,3 +38,17 @@ function longestConsec(strarr, k) {
   }
     return winner
 }
+
+// alt solution:
+// function longestConsec(strarr, k) {
+//   if (k <= 0 || k > strarr.length) {
+//     return "";
+//   }
+  
+//   return strarr
+//   .map((value, index) => (
+//     strarr.slice(index, index+k).join('')
+//   ))
+//   .reduce((longest, current) => current.length > longest.length ? current : longest)
+  
+// }
