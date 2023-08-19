072e3047dea1edd0f9a84a478653b1548afab71e
Todd Pangilinan <pang.todd@gmail.com>
2023-08-19 11:28:34 -0700

    added alt solution, used a for loop, but manipulated i to += int, clever.

--- codeWars/multiplesFactors.js
+++ codeWars/multiplesFactors.js
@@ -14,3 +14,12 @@ function findMultiples(integer, limit) {
   }
   return answer
 }
+
+// function findMultiples(int,limit){
+//   let result = []
+  
+//   for (let i = int; i<=limit ; i+=int)
+//     result.push(i)
+    
+//   return result
+// }
