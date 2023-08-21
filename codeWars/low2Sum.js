57f4e11a7435f7e6c3c788c8b35a65dedc07b688
Todd Pangilinan <pang.todd@gmail.com>
2023-08-21 16:09:04 -0700

    added alt solution, which was the only one not to use sort. It used Math.min & Math.max, and sorted through the array to find the lowest numbers. I considered using Math.min to find lowest number, remove it, and then run Math.min again to get the second lowest, but didn't see that solution.

--- codeWars/low2Sum.js
+++ codeWars/low2Sum.js
@@ -9,3 +9,23 @@ function sumTwoSmallestNumbers(numbers) {
   const sortedNumbers=numbers.sort((a,b)=>a-b)
   return sortedNumbers[0]+ sortedNumbers[1]
 }
+
+// alt solution:
+// function sumTwoSmallestNumbers(numbers) {  
+//   var smallestNumber = 0,
+//       secondSmallest = 0;
+  
+//   smallestNumber = Math.min(numbers[0], numbers[1]);
+//   secondSmallest = Math.max(numbers[0], numbers[1]);
+  
+//   for (var index = 2; index < numbers.length; index++) {
+//     if (numbers[index] < smallestNumber) {
+//       secondSmallest = smallestNumber;
+//       smallestNumber = numbers[index];
+//     } else if (numbers[index] < secondSmallest) {
+//       secondSmallest = numbers[index];
+//     }
+//   }
+  
+//   return (smallestNumber + secondSmallest);
+// };
