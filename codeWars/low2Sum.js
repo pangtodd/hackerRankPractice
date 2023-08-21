bb761a749c006233e9e2f7bf7d77e012e34b191c
Todd Pangilinan <pang.todd@gmail.com>
2023-08-21 15:53:19 -0700

    wrote passing solution using sort and then adding the first two elements of the array.

--- codeWars/low2Sum.js
+++ codeWars/low2Sum.js
@@ -4,3 +4,8 @@
 // For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
 
 // [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumTwoSmallestNumbers(numbers) {  
+  const sortedNumbers=numbers.sort((a,b)=>a-b)
+  return sortedNumbers[0]+ sortedNumbers[1]
+}
