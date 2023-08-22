372711c6fbd4cdbb817ec36a5f441333e1af70ed
Todd Pangilinan <pang.todd@gmail.com>
2023-08-21 23:20:53 -0700

    added alternate solution, kinda similar logic, but cleaner syntax.

--- codeWars/towerBuilder.js
+++ codeWars/towerBuilder.js
@@ -31,3 +31,14 @@ function towerBuilder(nFloors) {
   }
   return answer
 }
+
+// alt solution:
+// function towerBuilder(nFloors) {
+//   var tower = [];
+//   for (var i = 0; i < nFloors; i++) {
+//     tower.push(" ".repeat(nFloors - i - 1)
+//              + "*".repeat((i * 2)+ 1)
+//              + " ".repeat(nFloors - i - 1));
+//   }
+//   return tower;
+// }
