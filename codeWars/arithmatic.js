aa9ca56bbc197bad26b078b196ff3b5565c4e99d
Todd Pangilinan <pang.todd@gmail.com>
2023-08-22 14:23:46 -0700

    wrote passing solution without if statements, just used a chained ternary (also considered case statment.

--- codeWars/arithmatic.js
+++ codeWars/arithmatic.js
@@ -12,3 +12,7 @@
 // 5, 2, "multiply" --> 10
 // 5, 2, "divide"   --> 2.5
 // Try to do it without using if statements!
+
+function arithmetic(a, b, operator){
+  return operator == "add" ? a+b: operator =="subtract" ?a-b: operator == "multiply"? a*b :a/b 
+}
