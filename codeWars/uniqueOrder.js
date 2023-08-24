297692da5c988ce951ae6b97f065f1ddc20c6aec
Todd Pangilinan <pang.todd@gmail.com>
2023-08-24 15:07:17 -0700

    wrote passing solution using a for loop.

--- codeWars/uniqueOrder.js
+++ codeWars/uniqueOrder.js
@@ -6,3 +6,15 @@
 // uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 // uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 // uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+var uniqueInOrder = function(iterable) {
+  let current = "";
+  let answer = []
+  for (let i = 0; i < iterable.length; i++) {
+    if (iterable[i] != current) {
+      answer.push(iterable[i])
+      current = iterable[i]
+    }
+  }
+  return answer
+}
