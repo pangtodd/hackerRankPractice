04c2db2ab3989663d4c6d8e1b1c885daa937b493
Todd Pangilinan <pang.todd@gmail.com>
2023-08-23 18:48:49 -0700

    wrote passing solution using a loop to determine the number of capitlized letters, and then converting the string to either lower or uppcase string. But ignored the part about limiting the transformation to impacted letters. May try to refactor.

--- codeWars/fixString.js
+++ codeWars/fixString.js
@@ -8,3 +8,11 @@
 // solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
 // solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
 // solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+function solve(s){
+  let upperPos = 0;
+  for(let i=0; i<s.length;i++){
+    if(s[i].toUpperCase() == s[i]) upperPos+=1
+  };
+  return upperPos>s.length/2? s.toUpperCase(): s.toLowerCase()
+}
