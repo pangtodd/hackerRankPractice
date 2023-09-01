1631c85820b5d5ee57932b88080b52abf7caba6c
Todd Pangilinan <pang.todd@gmail.com>
2023-08-31 17:43:02 -0700

    wrote passing solution, which was just a clean nested for loop.

--- codeWars/multTable.js
+++ codeWars/multTable.js
@@ -22,3 +22,17 @@ multiplicationTable = function(size) {
   return multTable
 }
 
+// alt solution:
+// multiplicationTable = function(size) {
+//   var result = [];
+
+//   for (var i = 0; i < size; i++) {
+//     result[i] = [];
+//     for(var j = 0; j < size; j++) {
+//       result[i][j] = (i + 1) * (j + 1);
+//     }
+//   }
+  
+//   return result
+// }
+
