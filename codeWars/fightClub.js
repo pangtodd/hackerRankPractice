7450ac3def681d39c8690fef7c3eb4a35b60bba0
Todd Pangilinan <pang.todd@gmail.com>
2023-09-03 17:33:46 -0700

    added alt solution, which instead of running the scenerio out as a loop, used Math.ceil to compare fighter1.health/figther2.damagePerAttack and vice versa.

--- codeWars/fightClub.js
+++ codeWars/fightClub.js
@@ -42,3 +42,16 @@ firstAttacker = fighter1.name
 } 
 return fighter2.health>fighter1.health? fighter2.name : fighter1.name
 }
+
+// alt solution:
+// function declareWinner(fighter1, fighter2, firstAttacker) {
+//   var fac1 = Math.ceil( fighter1.health / fighter2.damagePerAttack );
+//   var fac2 = Math.ceil( fighter2.health / fighter1.damagePerAttack );
+//   if(fac1 < fac2) {
+//     return fighter2.name;
+//   } else if(fac2 < fac1) {
+//     return fighter1.name;
+//   } else {
+//     return firstAttacker;
+//   }
+// }
