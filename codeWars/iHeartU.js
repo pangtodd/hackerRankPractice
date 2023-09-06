1e93f7a727d2563b2553fc02b814fcccf6869d66
Todd Pangilinan <pang.todd@gmail.com>
2023-09-06 12:27:52 -0700

    added alt solution...I overcomplicated it w/my three conditionals- could have just focused on the % option.

--- codeWars/iHeartU.js
+++ codeWars/iHeartU.js
@@ -17,3 +17,17 @@ function howMuchILoveYou(nbPetals) {
   const phrase = ["I love you", "a little", "a lot","passionately","madly","not at all"]
   return nbPetals<7? phrase[nbPetals-1]: nbPetals%6 ==0? phrase[5]: phrase[nbPetals%6-1]
 }
+
+// alt solution:
+
+// function howMuchILoveYou(nbPetals) {
+//   let phrase = {    
+//     0: "not at all",
+//     1: "I love you",
+//     2: "a little",
+//     3: "a lot",
+//     4: "passionately",
+//     5: "madly"
+//   }
+//   return phrase[nbPetals%6]
+// }
