ed362f8c0b953d43f1b4b57d4b54c7b0e3d89d15
Todd Pangilinan <pang.todd@gmail.com>
2023-09-07 20:17:14 -0700

    wrote passing solution using for loop and a simple ternary.

--- codeWars/teamWeight.js
+++ codeWars/teamWeight.js
@@ -14,3 +14,11 @@
 // Explanation:
 // The first element 62 is the total weight of team 1, and the second element 27 is the total weight of team 2.
 
+function rowWeights(array){
+  let teamUno= 0;
+  let teamDos=0
+  for(let i=0; i<array.length; i++){
+    i%2==0? teamUno+=array[i]:teamDos+=array[i]
+    }
+  return [teamUno, teamDos]
+}
