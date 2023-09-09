e1ecd4cbeb5c5106c06584bda4446d87d38ef5e5
Todd Pangilinan <pang.todd@gmail.com>
2023-09-08 19:04:22 -0700

    wrote passing solution using a for loop and a conditional.

--- codeWars/countEachLetter.js
+++ codeWars/countEachLetter.js
@@ -2,3 +2,15 @@
 // The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 
 // What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {
+  let answer = {};
+  for (let i =0; i < string.length; i++){
+    if (answer[string[i]]) {
+        answer[string[i]] +=1 
+      } else {
+      answer[string[i]]=1;
+    }
+  }
+  return answer;
+}
