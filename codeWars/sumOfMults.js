eec8423cb2715b6e0a82b469d9993d48e062c11f
Todd Pangilinan <pang.todd@gmail.com>
2023-09-08 19:46:05 -0700

    wrote passing solution using a brute force loop...guessing there isa much more effective math forumla, but I couldn't think of it before I finished writing my solution.

--- codeWars/sumOfMults.js
+++ codeWars/sumOfMults.js
@@ -10,3 +10,15 @@
 // sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
 // sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
 // sumMul(4, -7)  ==> "INVALID"
+
+function sumMul(n,m){
+  if (n<1 || m<1) return "INVALID"
+  let score = 0;
+  let answer = 0;
+  while (score<m-n){
+    score += n;
+    answer += score;
+  }
+  return answer
+}
+
