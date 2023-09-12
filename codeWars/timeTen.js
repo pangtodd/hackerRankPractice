57bb975ee731776eb875f693ad93ac307999153c
Todd Pangilinan <pang.todd@gmail.com>
2023-09-11 17:00:56 -0700

    added alt solution, which was similar, but used a ternary to make sure the last /n got placed correctly.

--- codeWars/timeTen.js
+++ codeWars/timeTen.js
@@ -24,3 +24,14 @@ function multiTable(number) {
   }
   return answer.join("\n")
 }
+
+// alt soltuion:
+// const multiTable = (number) => {
+//   let table = '';
+  
+//   for(let i = 1; i <= 10; i++) {
+//     table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+//   }
+
+//   return table;
+// }
