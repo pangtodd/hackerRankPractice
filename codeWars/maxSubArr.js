9b48e5ef52eb208538ed7d01eca14e4bb5bb0673
Todd Pangilinan <pang.todd@gmail.com>
2023-09-14 18:49:27 -0700

    added alt solution, which used Kadane’s Algorithm. Need to study that.

--- codeWars/maxSubArr.js
+++ codeWars/maxSubArr.js
@@ -17,3 +17,14 @@ var maxSequence = function(arr) {
   }
   return max
 }
+
+// alt solution:
+// var maxSequence = function(arr){
+//   var min = 0, ans = 0, i, sum = 0;
+//   for (i = 0; i < arr.length; ++i) {
+//     sum += arr[i];
+//     min = Math.min(sum, min);
+//     ans = Math.max(ans, sum - min);
+//   }
+//   return ans;
+// }
