cc299f109b10f6c9ffd44c3329f0a66f46442785
Todd Pangilinan <pang.todd@gmail.com>
2023-09-15 19:35:24 -0700

    added alt solution, which sets up a separate helper function and uses a switch case- kinda similar logic otherwise, but cleaner/easier to read. All other solutions used reg ex to manipulate strings/elements.

--- codeWars/encryptThis.js
+++ codeWars/encryptThis.js
@@ -36,3 +36,20 @@ let encryptThis = function(text){
   }
   return answer.join(" ")
 }
+
+// alt solution:
+
+// const encryptWord = w => {
+//   const first = w.charCodeAt(0);
+//   let res;
+//   switch (w.length) {
+//     case 0: return '';
+//     case 1: return first;
+//     case 2: res = [first, w[1]]; break;
+//     case 3: res = [first, w[2], w[1]]; break;
+//     default: res = [first, w.slice(-1), w.slice(2,-1), w[1]]; break;
+//   }
+//   return res.join('');
+// };
+
+// const encryptThis = text => text.split(' ').map(encryptWord).join(' ');
