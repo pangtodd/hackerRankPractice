70a8b37b5421048832541971a446acc6c600dd50
Todd Pangilinan <pang.todd@gmail.com>
2023-09-15 19:22:53 -0700

    wrote a (long-winded) passing solution using charCodeAt, a for loop, and a bunch of text manipulation with slice.

--- codeWars/encryptThis.js
+++ codeWars/encryptThis.js
@@ -17,4 +17,22 @@
 // encryptThis("good") === "103doo"
 // encryptThis("hello world") === "104olle 119drlo"
 
+let encryptThis = function(text){
+  let answer = []
+  if (text.length < 2) return text.charCodeAt(0).toString()
+  let textArr = text.split(" ")
+  for (let i = 0; i < textArr.length; i++) {
+    let element = textArr[i]
+    if (element.length < 2) {
+      answer.push(element[0].charCodeAt(0))
+    } else {
+      let holder = textArr[i][textArr[i].length - 1]
+      element = element.slice(0, element.length - 1) + textArr[i][1]
+      element = element[0] + holder + element.slice(2, )
+      element = textArr[i][0].charCodeAt(0).toString() + element.slice(1, )
+      answer.push(element)
+    }
 
+  }
+  return answer.join(" ")
+}
