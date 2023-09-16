12138fa31d8f064d3b10c6ad25821a33923b45be
Todd Pangilinan <pang.todd@gmail.com>
2023-09-15 20:37:58 -0700

    added alt solution which used straight math (Math.log) instead of a loop.

--- codeWars/evap.js
+++ codeWars/evap.js
@@ -19,3 +19,10 @@ function evaporator(content, evap_per_day, threshold){
   }
   return score
 }
+
+// alt solution:
+// function evaporator(content, evap_per_day, threshold){ 
+//   threshold = threshold / 100
+//   evap_per_day = evap_per_day / 100
+//   return  Math.ceil(Math.log(threshold) / Math.log(1-evap_per_day))
+// }
