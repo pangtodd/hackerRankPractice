bf53c879de6cee3e98161e55ca9fd22585176d3e
Todd Pangilinan <pang.todd@gmail.com>
2023-09-19 14:41:46 -0700

    added alt solution, which used two loops but had similar logic otherwise. The top solutions were more like mine, whoo hoo

--- codeWars/sortOddsOnly.js
+++ codeWars/sortOddsOnly.js
@@ -10,3 +10,27 @@ function sortArray(array) {
   let sortedArr = array.filter((e)=>e%2!=0).sort((a,b)=>a-b);
   return array.map((e)=> e%2!=0? sortedArr.shift():e)
   }
+
+// alt solution:
+// function sortArray(array) {
+//   var odds = [];
+//   //loop, if it's odd, push to odds array
+//   for (var i = 0; i < array.length; ++i) {
+//     if (array[i]%2 !== 0) {
+//       odds.push(array[i]);
+//     }
+//   }
+//   //sort odds from smallest to largest
+//   odds.sort(function(a,b){
+//     return a-b;
+//   });
+  
+//   //loop through array, replace any odd values with sorted odd values
+//   for (var j = 0; j < array.length; ++j) {
+//     if (array[j]%2 !== 0) {
+//       array[j] = odds.shift();
+//     }
+//   }
+  
+//  return array;
+// }
