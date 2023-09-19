e738c3df4987497cc5c551e70275187a2069763c
Todd Pangilinan <pang.todd@gmail.com>
2023-09-19 14:12:35 -0700

    added alt solution which uses a stack to keep track/pairs- only adds opening brackets to the stack, when it encounters a closing bracket, it removes the matching opening bracket from the end of the stack.

--- codeWars/validBrackets.js
+++ codeWars/validBrackets.js
@@ -67,6 +67,7 @@ var isValid = function(s) {
   return s.length==0
 };
 
+// Same thing, but using Reg Ex:
 // var isValid = function(s) {
 //   if (s.length % 2 !== 0) return false;
 //   const pairRegex = /\(\)|\[\]|\{\}/g;
@@ -74,5 +75,34 @@ var isValid = function(s) {
 //     s = s.replace(pairRegex, "");
 //   }
 //   return s.length === 0;
+// };
+
+alt solution:
+const pairs = {
+  "(": ")",
+  "[": "]",
+  "{": "}"
+}
+   
+// var isValid = function(s) {
+  
+//   // check if length is an odd number. if it is, return false
+//   // if there is an odd number, it means that at least one bracket is missing a pair
+//   if (s.length % 2 === 1) return false
+//   if (s[0] === "]" || s[0] === ")" || s[0] === "}") return false
+//   if (s[s.length - 1] === "[" || s[s.length - 1] === "(" || s[s.length - 1] === "{") return false
+  
+//   let stack = []
+  
+//   for(let i=0; i<s.length; i++) {
+//       if(s[i] === "[" || s[i] === "(" || s[i] === "{") {
+//           stack.push(s[i])
+//       } else if (pairs[stack.pop()] !== s[i]) {
+//           return false
+//       }
+      
+//   }
+//   return stack.length === 0
+  
 // };
 
