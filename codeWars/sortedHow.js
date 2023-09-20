fdf2b84d8c3b544fa22c93e94a9c4bfdfd6de2c3
Todd Pangilinan <pang.todd@gmail.com>
2023-09-20 14:15:05 -0700

    wrote passing solution- I assume if two elements are equal, that doesn't disrupt it being sorted ascending or descending. Used a basic loop to compare i vs i+1.

--- codeWars/sortedHow.js
+++ codeWars/sortedHow.js
@@ -5,3 +5,18 @@ prompt:
 // "yes, descending" - if the numbers in the array are sorted in a descending order
 // "no" - otherwise
 // You can assume the array will always be valid, and there will always be one correct answer.
+
+function isSortedAndHow(array) {
+  let asc = [];
+  let desc =[];
+  for (let i=0; i<=array.length; i++){    
+    if(array[i]<array[i+1]){
+      desc.push("false");
+    }
+    if(array[i]>array[i+1]){
+        asc.push("false");
+      }
+  }
+  if(asc.length>0 && desc.length>0) return "no"
+  return desc.length>0?"yes, ascending":"yes, descending"
+}
