0c81cb4242e8598a5d3fc11be908ecfa6a040733
Todd Pangilinan <pang.todd@gmail.com>
2023-09-22 16:51:13 -0700

    wrote passing solution with a case switch statement and toLowerCase().

--- codeWars/drinks.js
+++ codeWars/drinks.js
@@ -12,3 +12,27 @@
 // Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
 
 // Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+
+function getDrinkByProfession(param){
+  const lower = param.toLowerCase()
+  switch(lower){
+    case "jabroni":
+      return "Patron Tequila"
+      break;
+    case "school counselor":
+      return "Anything with Alcohol"
+      break;
+    case "programmer":
+      return "Hipster Craft Beer"
+      break;
+    case "bike gang member":
+      return "Moonshine"
+      break;
+    case "politician":
+      return "Your tax dollars"
+    case "rapper":
+      return "Cristal"
+    default:
+      return "Beer"
+  }
+}
