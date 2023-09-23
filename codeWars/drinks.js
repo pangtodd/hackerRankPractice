d2aab401469d4f559912a913e1edd6ef6b6e6c10
Todd Pangilinan <pang.todd@gmail.com>
2023-09-22 17:14:20 -0700

    added alt solution, which just built an object connecting the key/values.

--- codeWars/drinks.js
+++ codeWars/drinks.js
@@ -36,3 +36,15 @@ function getDrinkByProfession(param){
       return "Beer"
   }
 }
+
+// alt solution:
+// const drinks = {
+//   "jabroni": "Patron Tequila",
+//   "school counselor": "Anything with Alcohol",
+//   "programmer": "Hipster Craft Beer",
+//   "bike gang member": "Moonshine",
+//   "politician": "Your tax dollars",
+//   "rapper": "Cristal"
+// }
+
+// const getDrinkByProfession = profession => drinks[profession.toLowerCase()] || "Beer"
