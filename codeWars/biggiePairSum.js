18aa91c09fa74eaba31250f6a076300e558de47d
Todd Pangilinan <pang.todd@gmail.com>
2023-09-26 07:49:34 -0700

    added alt solution, which used sort.

--- codeWars/biggiePairSum.js
+++ codeWars/biggiePairSum.js
@@ -34,3 +34,9 @@ function largestPairSum(numbers) {
   }
   return biggie + secondBiggie;
 }
+
+// alt solution:
+// function largestPairSum(numbers){
+//   numbers.sort(function(a, b){ return b - a });
+//   return numbers[0] + numbers[1];
+// }
