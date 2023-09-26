104d9d8d9b610f9797d5ae06db80273e8a732e10
Todd Pangilinan <pang.todd@gmail.com>
2023-09-25 18:28:10 -0700

    wrote passing solution using filter and slice.

--- codeWars/shortEvenFilter.js
+++ codeWars/shortEvenFilter.js
@@ -6,3 +6,8 @@
 // ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
 // ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
 // ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+
+function evenNumbers(array, number) {
+  let evens = array.filter((e)=>e%2==0)
+  return evens.slice(evens.length-number,)
+}
