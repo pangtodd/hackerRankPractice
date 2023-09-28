1139f5c6791e7d1c39997580415f6ef4b0f96ff4
Todd Pangilinan <pang.todd@gmail.com>
2023-09-27 17:25:58 -0700

    wrote passing solution using Math.pow and Math.PI.

--- codeWars/piAreaSquare.js
+++ codeWars/piAreaSquare.js
@@ -2,3 +2,7 @@
 // Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
 // Graph- a square with a red arc, which is 1/4 of a circle, starting at the bottom left corner to the top right corner.
 // Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+
+function squareArea(A){
+  return Math.pow(A*4/(2*Math.PI), 2)
+} 
