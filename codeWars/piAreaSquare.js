e8417b4b3fa5ffc1c4030c4861a23b5b7e2a1889
Todd Pangilinan <pang.todd@gmail.com>
2023-09-27 17:29:37 -0700

    added alt solution, which had similar logic to mine, but is layed out with more detail and rounds correctly (i forgot that step but still passed?)

--- codeWars/piAreaSquare.js
+++ codeWars/piAreaSquare.js
@@ -6,3 +6,11 @@
 function squareArea(A){
   return Math.pow(A*4/(2*Math.PI), 2)
 } 
+
+// alt solution:
+// function squareArea(A){
+//   var circum = 4 * A;
+//   var radius = circum / (2 * Math.PI);
+//   var area = Math.pow(radius, 2);
+//   return Math.round(area*100)/100
+// }
