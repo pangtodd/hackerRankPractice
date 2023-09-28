5ab7a0429f90865496ae357bf6a1dda31562b893
Todd Pangilinan <pang.todd@gmail.com>
2023-09-27 17:56:35 -0700

    wrote a solution with a for loop. Considered recursive function, but that seemed less efficient.

--- codeWars/basicFactoral.js
+++ codeWars/basicFactoral.js
@@ -2,3 +2,12 @@
 // Your task is to write function factorial.
 
 // https://en.wikipedia.org/wiki/Factorial
+
+function factorial(n){
+  //   return n > 1 ? n * factorial(n - 1) : 1;
+    let answer= 1
+    for(let i =1; i<=n; i++){
+      answer*=i
+    }
+    return answer
+  }
