13a8f327545a3f79a2716161184da17fd0c3ab6f
Todd Pangilinan <pang.todd@gmail.com>
2023-09-28 20:05:47 -0700

    wrote passing solution with switch case and template literals.

--- codeWars/whoLikes.js
+++ codeWars/whoLikes.js
@@ -10,3 +10,21 @@
 // ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 // Note: For 4 or more names, the number in "and 2 others" simply increases.
 
+function likes(names) {
+  switch(names.length){
+    case 0:
+      return "no one likes this"
+      break;
+    case 1:
+      return `${names[0]} likes this`
+      break;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`
+      break;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`
+      break;
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length -2} others like this`
+  }
+}
