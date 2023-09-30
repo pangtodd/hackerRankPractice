6fe892ed1db7f68d2fd7c0315f057036d7759ee5
Todd Pangilinan <pang.todd@gmail.com>
2023-09-29 18:36:48 -0700

    added alt solution, which is a bit lengthier, but probably more efficient, just uses conditionals to figure out which position is in the middle.

--- codeWars/middleElement.js
+++ codeWars/middleElement.js
@@ -20,3 +20,18 @@ function gimme (triplet) {
 //   const smalls = Math.min(...triplet)
 //   return triplet.findIndex((e)=> e<biggie && e>smalls)
 }
+
+// alt solution:
+// var gimme = function (inputArray) {
+//   if ((inputArray[0] < inputArray[1] && inputArray[0] > inputArray[2])
+//     || (inputArray[0] > inputArray[1] && inputArray[0] < inputArray[2]))
+//     return 0;
+    
+//   if ((inputArray[1] < inputArray[0] && inputArray[1] > inputArray[2])
+//     || (inputArray[1] > inputArray[0] && inputArray[1] < inputArray[2]))
+//     return 1;
+    
+//   if ((inputArray[2] < inputArray[0] && inputArray[2] > inputArray[1])
+//     || (inputArray[2] > inputArray[0] && inputArray[2] < inputArray[1]))
+//     return 2;
+// };
