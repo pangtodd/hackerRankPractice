585ab741e9fea42cd1d6a6409f849c2856cffcf4
Todd Pangilinan <pang.todd@gmail.com>
2023-10-02 17:08:22 -0700

    added alt solution, which is similar except it used map instead of a loop...and just looks way more clean.

--- codeWars/alphaWar.js
+++ codeWars/alphaWar.js
@@ -43,3 +43,10 @@ for(let i=0;i<fight.length; i++){
 }
 return score == 0 ? "Let's fight again!" : score>0? "Left side wins!": "Right side wins!"
 }
+
+// alt solution:
+// function alphabetWar(fight) {
+//   let map = { w: -4, p: -3, b: -2, s: -1, m: 4, q: 3, d: 2, z: 1 };
+//   let result = fight.split('').reduce((a, b) => a + (map[b] || 0), 0);
+//   return result ? (result < 0 ? "Left" : "Right") + " side wins!" : "Let's fight again!";
+// }
