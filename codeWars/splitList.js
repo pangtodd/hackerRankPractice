540af2e9d59e197cdf5e276f52eb3ada89a6153e
Todd Pangilinan <pang.todd@gmail.com>
2023-10-04 18:25:12 -0700

    added alt solution, which managed to use map instead of a loop.

--- codeWars/splitList.js
+++ codeWars/splitList.js
@@ -27,3 +27,8 @@ function partlist(arr) {
     }
   return answer
 }
+
+// alt solution:
+// function partlist(arr) {
+//   return arr.map((v, i) => [arr.slice(0, i).join(' '), arr.slice(i).join(' ')]).slice(1)
+// }
